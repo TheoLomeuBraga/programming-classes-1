7c231cd557dbd0d5d536f893391d1658ded6317d
Théo <63592879+TheoLomeuBraga@users.noreply.github.com>
2026-02-17 19:54:01 -0300

    update

--- main_charter/main_charter_model.gd
+++ main_charter/main_charter_model.gd
@@ -1,2 +1,9 @@
 extends Node3D
 class_name MainCharterModel
+
+@export var color_1 : Color
+@export var color_2 : Color
+@export var color_3 : Color
+@export var color_4 : Color
+@export var color_5 : Color
+@export var color_6 : Color
